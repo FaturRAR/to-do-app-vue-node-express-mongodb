f2aac86ae0c20540acc3b0552908c55ac154bde3
FaturRAR <akunbelajarfatur@gmail.com>
2020-10-01 10:24:34 +0700

    master

--- server.js
+++ server.js
@@ -11,19 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-// const whitelist = ['https://todoapprarr.web.app', 'https://null.jsbin.com', 'https://front-end-vue-3a743.web.app', 'http://127.0.0.1:5500']
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-
 const config = {
-  "origin": "http://localhost:8081",
+  "origin": "https://vuetodorar.web.app/",
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
   "optionsSuccessStatus": 200
@@ -38,6 +27,7 @@ connect()
 
 const router = require('./app/router/router')
 app.use('/api/', router)
+
 const db = require('./app/model/list.model');
 // const todo = require('./app/controller/controller');
 app.get('/api',  (req, res) => {
