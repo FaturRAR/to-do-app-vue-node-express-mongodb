ba9c272adc191c541a46716fc8ad6d4c7229ea0f
FaturRAR <akunbelajarfatur@gmail.com>
2020-10-01 09:36:25 +0700

    master

--- app/db/connect.js
+++ app/db/connect.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const URI = process.env.MONGO_URI || 'mongodb+srv://root:root@cluster0.lx09x.gcp.mongodb.net/test?retryWrites=true&w=majority'
+const URI =  'mongodb+srv://root:root@cluster0.lx09x.gcp.mongodb.net/test?retryWrites=true&w=majority'
 
 const connectDb = async () => {
-    await mongoose.connect(URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true, 
         useFindAndModify: false
